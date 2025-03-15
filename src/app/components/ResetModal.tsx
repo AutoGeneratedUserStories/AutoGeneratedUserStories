72dc150f4b555b80097e8dff0bacee81b603277b
Marrufof <fm8191@rit.edu>
2025-03-14 22:50:21 -0400

    added a close button on reset modal, needs ui fixing

--- src/app/components/ResetModal.tsx
+++ src/app/components/ResetModal.tsx
@@ -29,6 +29,12 @@ const ResetModal: React.FC<ResetModalProps> = ({ isOpen, onClose, onSaveAndReset
           >
             Delete & Reset
           </button>
+            <button
+                className="px-4 py-2 bg-gray-300 text-gray-700 rounded-lg hover:bg-gray-400"
+                onClick={onClose}
+            >
+                Cancel
+            </button>
         </div>
 
         <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700" onClick={onClose}>
